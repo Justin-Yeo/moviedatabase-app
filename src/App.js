0ef130d816fa9ec3b4aef9623e110be7c31073e9
Justin-Yeo <156579337+Justin-Yeo@users.noreply.github.com>
2024-08-04 22:43:48 +0800

    Add Result and Style for Result

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
-import React from 'react'; 
+import React, {useState} from 'react'; 
+import axios from 'axios'
 import Search from './components/Search'
+import Results from './components/Results'
+
 
 function App() {
+  const [state, setState] = useState({
+    results : [], 
+    selected: {}
+  });
+  const apirul = "http://www.omdbapi.com/?i=tt3896198&apikey=ce17eac6"
+
+  const search = (e) => {
+    if (e.key === "Enter") {
+      axios(apirul + "&s=" + state.s).then(({data}) => {
+        let results = data.Search; 
+
+        setState(prevState => {
+          return {...prevState, results: results}
+        })
+      });
+    }
+  }
+  const handleInput = (e) => {
+    let s = e.target.value; 
+
+    setState(prevState => {
+      return {...prevState, s: s}
+    }); 
+  }
+
   return (
     <div className="App">
       <header>
         <h1>Movie App</h1>
       </header>
       <main>
-        <Search />
+        <Search handleInput={handleInput} search={search} />
+        <Results results={state.results} />
       </main>
     </div>
   );
